4cc23f1bfec00cee9572826b8c6d0dee91f89da0
Coralie <immacora@gmail.com>
2022-12-29 14:12:10 +0100

    Ajout du fichier de constantes

---
+++ js/const.js
@@ -0,0 +1,8 @@
+/* CONSTANTES */
+
+// URL
+const ocMoviesURL = "http://localhost:8000/api/v1/titles/";
+const bestMoviesFilter = "?sort_by=-imdb_score";
+const categoryFantasy = "&genre=fantasy";
+const categoryDocumentary = "&genre=documentary";
+const categoryThriller = "&genre=thriller";
