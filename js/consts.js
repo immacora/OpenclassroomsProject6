597a7cc11bc0c12d2ae46638a4b8730df8e9ae45
Coralie <immacora@gmail.com>
2023-01-11 18:21:34 +0100

    constantes

---
+++ js/consts.js
@@ -0,0 +1,18 @@
+/* CONSTANTES */
+
+// CATEGORIE NAME
+const fantasyName = 'fantasy';
+const documentaryName = 'documentary';
+const thrillerName = 'thriller';
+
+// API FILTERS
+const bestMoviesFilter = '?sort_by=-imdb_score';
+const categoryFilter = '&genre=';
+
+// URL
+const ocMoviesURL = 'http://localhost:8000/api/v1/titles/';
+
+export const bestMoviesURL = ocMoviesURL + bestMoviesFilter;
+export const bestMoviesFantasyURL = bestMoviesURL + categoryFilter + fantasyName;
+export const bestMoviesDocumentaryURL = bestMoviesURL + categoryFilter + documentaryName;
+export const bestMoviesThrillerURL = bestMoviesURL + categoryFilter + thrillerName;
