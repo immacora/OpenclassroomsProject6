cfd295dc7acd5c0bd5c7db79d5f6a1bf468d061e
Coralie <immacora@gmail.com>
2023-01-20 14:37:56 +0100

    Création du fichier hero

---
+++ js/hero.js
@@ -0,0 +1,29 @@
+import { getMovieInfos } from './requests.js';
+import { createMovieCard } from './movie_card_and_modal.js';
+
+/**
+ * Création de l'affichage du Hero
+ * @param { Object } bestMoviesIds
+ */
+export async function createHero(bestMoviesIds) {
+    try {
+        // Récupération des données du film
+        const movieId = bestMoviesIds[0];
+        const movieInfos = await getMovieInfos(movieId);
+        const movieTitle = movieInfos.title;
+        const movieLongDescription = movieInfos.long_description;
+
+        // Rattachement des éléments à l'élément parent du DOM
+        document.querySelector('.movie_summary__title').innerHTML = movieTitle;
+        document.querySelector('.movie_summary__long_description').innerHTML = movieLongDescription;
+
+        // Déclaration de l'id cible du DOM pour insertion de la movieCard
+        const heroMovieDomId = 'hero';
+
+        // Création de la movieCard
+        createMovieCard(movieInfos, heroMovieDomId, movieTitle);
+
+    } catch (error) {
+        alert("Erreur d'affichage du meilleur film toutes catégories confondue" + error);
+    }
+}
